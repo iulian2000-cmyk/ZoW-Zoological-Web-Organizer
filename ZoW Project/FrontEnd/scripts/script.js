7dca2dd928e1faea5743701a98c825de2d6a2819
iulian2000-cmyk <iuliangeorge581@yahoo.ro>
2021-04-09 16:39:04 +0300

    Changes Documentation

--- ZoW Project/FrontEnd/scripts/script.js
+++ ZoW Project/FrontEnd/scripts/script.js
@@ -1,41 +1,73 @@
 //console.log("Script pagina principala!");
 
+var displayMenu = 0;
+var displaySettings = 0;
 
 function showMenu() {
-    document.getElementById('menu').innerHTML = '<div class="usermenu">' +
-        '<p> <strong> <i> User menu </i> </strong> </p> <br> <br> <br> ' +
-        '<p> <strong> <i>  &nbsp; Habitat </i> </strong> </p> <br> <br> ' +
-        '<select name=\"habitat\" id=\"habitat\">' +
-        '<option value=\"val1\">val1</option>' +
-        '<option value=\"val2\">val2</option>' +
+    var menu = document.getElementById('menu');
+    menu.innerHTML = '<div class="sidenav">' +
+        '<p> User menu </p>' +
+        '<button id="generateStats">Generate stats </button>' +
+        '<button id="generateOrder">Generate clasament</button>' +
+        '<button class="habitat"> Habitat ' +
+        '<form>' +
+        '<select id="habitat" > ' +
+        '<option val=1> value1 </option>' +
+        '<option val=2> value 2</option>' +
         '</select>' +
-        '<p> <strong> <i> &nbsp; &nbsp; Tip </i> </strong> </p> <br> <br> <br>' +
-        '<select name=\"tip\" id=\"tip\">' +
-        '<option value=\"val1\">val1</option>' +
-        '<option value=\"val2\">val2</option>' +
+        '</form>' +
+        '</button> <br> ' +
+        '<button class="type"> Type ' +
+        '<form>' +
+        '<select id="type" > ' +
+        '<option val=1> value1 </option>' +
+        '<option val=2> value 2</option>' +
         '</select>' +
-        '<p> <strong> <i>Longevitate </i> </strong> </p> <br> <br><br><br>' +
-        '<select name=\"longevitate\" id=\"longevitate\">' +
-        '<option value=\"val1\">val1</option>' +
-        '<option value=\"val2\">val2</option>' +
+        '</form>' +
+        '</button> <br> ' +
+        '<button class="longevitate">  Longevitate ' +
+        '<form>' +
+        '<select id="longevitate" > ' +
+        '<option val=1> value1 </option>' +
+        '<option val=2> value 2</option>' +
         '</select>' +
-        '<button type=\"button\" id=\"generateAlbum\"> Generate album </button>' +
-        '<button type=\"button\" id=\"generateStats\"> Generate statistics </button>' +
-        '<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>' +
-        '<p> <strong <i> Choose album </i> </strong> </p> <br> <br> <br>' +
-        '<select name=\"chooseAlbum\" id=\"choosealbum\">' +
-        '<option value=\"Pasari\">Pasari</option>' +
-        '<option value=\"Mamifere\"> Mamifere</option>' +
-        '<option value=\"Acvatice\"> Acvatice </option>' +
-        '<option value=\"Reptile\"> Reptile </option>' +
-        '</select>' +
-        '<br> <br>' +
-        '<p> <strong> <i> My albums </i> </strong> </p> <br> <br> <br>' +
-        '<select name=\"chooseMyAlbum\" id=\"chooseMyalbum\">' +
-        '<option value=\"Album1\">Album1</option>' +
-        '<option value=\"Album2\"> Album2</option>' +
-        '</select>' +
-        '<br> <br> <br> ' +
-        '<button type=\"button\" id=\"ShowAlbum\"> Show album </button>'
-    '</div>';
+        '</form>' +
+        '</button> <br> ' +
+        '<button id="generateAlbum"> Generare album </button>' +
+        '<div class="dropdown1">' +
+        '<button id="predefinedAlbums"> Albume predefinite </button>' +
+        '<div class="dropdown-content1">' +
+        '<a href="#"> Pasari </a>' +
+        '<a href="#"> Reptile </a>' +
+        '<a href="#"> Mamifere </a>' +
+        '<a href="#"> Acvatice </a>' +
+        '</div>' +
+        '</div>' +
+        '<div class="dropdown2">' +
+        '<button id="myAlbums"> Albumele mele </button>' +
+        '<div class="dropdown-content2">' +
+        '<a href="#"> Album1  </a>' +
+        '<a href="#"> Album2 </a>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
+    if (displayMenu == 0) {
+        menu.style.display = "none";
+        displayMenu = 1;
+    } else {
+        displayMenu = 0;
+        menu.style.display = "block";
+    }
+}
+
+function showSettings() {
+    var settings = document.getElementById('settings');
+    settings.innerHTML = '<a href="#"> Logout </a>';
+    if (displaySettings == 0) {
+        settings.style.display = "none";
+        displaySettings = 1;
+    } else {
+        settings.style.display = "block";
+        displaySettings = 0;
+    }
 }
