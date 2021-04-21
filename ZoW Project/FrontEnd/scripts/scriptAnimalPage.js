50bd1c2f94e3e5da0934cd793be20f1790b71fd7
Silviu <dumitru.silviu21@gmail.com>
2021-04-21 21:03:21 +0300

    Update
    am adaugat navbarul la pagina leu.html

--- ZoW Project/FrontEnd/scripts/scriptAnimalPage.js
+++ ZoW Project/FrontEnd/scripts/scriptAnimalPage.js
@@ -1,32 +1,43 @@
 var slideIndex = 1;
 
+var activeLog = true;
+
+function showLog() {
+    var log = document.getElementsByClassName("log")[0];
+    if (activeLog == true) {
+        log.style.display = "flex";
+        activeLog = false;
+    } else {
+        log.style.display = "none";
+        activeLog = true;
+    }
+}
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("Slides");
-  var dots = document.getElementsByClassName("demo");
+    var i;
+    var slides = document.getElementsByClassName("Slides");
+    var dots = document.getElementsByClassName("demo");
 
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
 
 
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-     
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  
-  dots[slideIndex-1].className += " active";
-  slides[slideIndex-1].style.display = "block";
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    dots[slideIndex - 1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
 }
-
