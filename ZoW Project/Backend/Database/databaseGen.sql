ceb657d0fe974f90f4dba23fa9ea9f4f29f350ae
RobertaNechita <roberta.nechita@info.uaic.ro>
2021-05-12 16:40:30 +0300

    Update databaseGen.sql

--- ZoW Project/Backend/Database/databaseGen.sql
+++ ZoW Project/Backend/Database/databaseGen.sql
@@ -25,10 +25,11 @@ CREATE TABLE animals(
     reptila BOOLEAN NOT NULL,
     dinozaur BOOLEAN NOT NULL,
     domestic BOOLEAN NOT NULL,
+    insecta BOOLEAN NOT NULL,
     salbatic BOOLEAN NOT NULL,
     comestibil BOOLEAN NOT NULL,
     incomestibil BOOLEAN NOT NULL,
-    longevitate BOOLEAN NOT NULL,
+    longevitate int NOT NULL,
     generalities TEXT NOT NULL,
     stiatiCa TEXT NOT NULL,
     cardLongevitate VARCHAR(64) NOT NULL,
@@ -66,3 +67,43 @@ CREATE TABLE addAnimalToAlbum(
     FOREIGN KEY (id_animal) REFERENCES animals(id_animal), 
     FOREIGN KEY (id_albumCustom) REFERENCES customAlbums(id_albumCustom)
 );  
+
+
+----------- populating the table --------------
+INSERT into animals 
+       VALUES ( 0,
+                'AFIDE',
+                '../pages/animalPage.html',
+                '../images/afide/afide1.jpg',
+                '../images/afide/afide2.jpg',
+                '../images/afide/afide3.jpg',
+                '../images/afide/afide4.jpg',
+                0,
+                TRUE,
+                'FALSE',
+                'FALSE',
+                'FALSE',
+                'FALSE',
+                'FALSE',
+                'FALSE',
+                'FALSE',
+                'TRUE',
+                'TRUE',
+                'FALSE',
+                'TRUE',
+                2,
+                'Afide – fac parte din ordinul Homoptera, familia Aphididae. Insectele din acest ordin au corpul mic sau mijlociu, capul hipogonat, aparatul bucal de intepat si supt, subtipul homopter. Antenele sunt formate
+                din 5 – 6 articole, foarte rar 3 – 4, lungi. Picioarele sunt adaptate pentru mers, iar la unele specii (purici, cicade etc.) cele posterioare sunt adaptate pentru sarit.Este o specie raspandita in aproape toate tarile globului iar in tara noastra o intalnim din zonele de campie pana in cele muntoase, in camp, gradini si spatii protejate. Este considerat un daunator polifag,
+                fiind semnalat pe numeroase plante cultivate si spontane insa pagube deosebite produce la culturile de castraveti, pepeni, dovlecei, bumbac, unele plante de sera.Paduchii solanaceelor ataca peste 200 de specii de plante ce includ legume ( tomate, cartof, salata, vinete, varza ), pomi fructiferi, plante ornamentale ( trandafiri ) si buruieni. Adultii si larvele produc
+                pagube prin inteparea frunzelor apicale pe partea inferioara, ceea ce duce la gofrarea frunzelor si distrugerea mugurilor de crestere .Filoxera este un daunator cunoscut in toate podgoriile din tara. Se prezinta sub 4 forme, ce se deosebesc intre ele prin caractere morfologice distincte. Doar doua prezinta importanta mai mare: filoxera radicicola
+                (pe radacini), forma foarte periculoasa si filoxera galicola (pe frunze).',
+                'Paduchele negru al ciresului – Myzus cerasi: Specie intalnita frecvent in livezile de ciresi si visini, provocand pagube mari in pepiniere.
+                Paduchele verde al piersicului, Myzodes persicae este o specie raspandita in Europa, Asia, America de Nord, Africa si Australia. In tara noastra se intalneste frecvent in toate regiunile.
+                Adultii si larvele acestei specii ataca marul. Paduchii colonizeaza la inceput mugurii floriferi si vegetativi, apoi, frunzele si fructele tinere.',
+                '1-2 luni',
+                'Insecte',
+                '0.01g',
+                '0,1cm',
+                'Japonia',
+                'ierbivor'
+            );                                                                                   
