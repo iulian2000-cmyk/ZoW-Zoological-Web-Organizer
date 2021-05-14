735fbd3d569f8b5e3b27c8d96597a20df43f2cc1
iulian2000-cmyk <67906958+iulian2000-cmyk@users.noreply.github.com>
2021-05-14 14:54:54 +0300

    Update download_service.js

--- ZoW Project/Backend/services/download_service.js
+++ ZoW Project/Backend/services/download_service.js
@@ -5,8 +5,32 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 var mysql = require('mysql');
 var XMLWriter = require('xml-writer');
+var http = require('http');
+var got = require('got');
 
 
-exports.download_xml(req, res) {
-    console.log("I AM HERE");
+exports.download_xml = function(req, res) {
+
+    var PathToXML = __dirname + url.parse(req.url).pathname;
+    if (process.platform == "win32") {
+        PathToXML = PathToXML.replace("\\services\\FrontEnd\\pages\\get_Order", "");
+        PathToXML = PathToXML + "\\FilesToDownload\\Order.xml";
+    } else {
+        PathToXML = PathToXML.replace("/services/FrontEnd/pages/get_Order", "");
+        PathToXML = PathToXML + "/FilesToDownload/Order.xml";
+    }
+    fs.exists(PathToXML, function(exists) {
+        if (exists) {
+            // Content-type is very interesting part that guarantee that
+            // Web browser will handle response in an appropriate manner.
+            res.writeHead(200, {
+                "Content-Type": "application/octet-stream",
+                "Content-Disposition": "attachment; filename=" + 'Order.xml'
+            });
+            fs.createReadStream(PathToXML).pipe(res);
+        } else {
+            res.writeHead(400, { "Content-Type": "text/plain" });
+            res.end("ERROR File does not exist");
+        }
+    });
 }
