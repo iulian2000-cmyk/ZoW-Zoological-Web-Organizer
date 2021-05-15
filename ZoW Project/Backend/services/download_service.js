b7b2b9501446e4d8d67acc2b0bbf7cb880e523e1
RobertaNechita <roberta.nechita@info.uaic.ro>
2021-05-15 16:56:11 +0300

    Update download_service.js

--- ZoW Project/Backend/services/download_service.js
+++ ZoW Project/Backend/services/download_service.js
@@ -6,6 +6,8 @@ const { JSDOM } = jsdom;
 var mysql = require('mysql');
 var XMLWriter = require('xml-writer');
 var http = require('http');
+const fastcsv = require('fast-csv');
+
 
 
 exports.download_xml = function(req, res) {
@@ -25,6 +27,51 @@ exports.download_xml = function(req, res) {
                 "Content-Disposition": "attachment; filename=" + "Order.xml"
             });
             fs.createReadStream(PathToXML).pipe(res);
+        } else {
+            res.writeHead(400, { "Content-Type": "text/plain" });
+            res.end("ERROR File does not exist");
+        }
+    });
+}
+
+
+exports.download_csv = function (req, res){
+    var PathToCSV = __dirname + url.parse(req.url).pathname;
+   
+    if (process.platform == "win32") {
+        PathToCSV = PathToCSV.replace("\\services/FrontEnd/pages/get_OrderCSV", "");
+        PathToCSV = PathToCSV + "\\FilesToDownload\\Order.csv";
+    } else {
+        PathToCSV = PathToCSV.replace("/services/FrontEnd/pages/get_OrderCSV", "");
+        PathToCSV = PathToCSV + "/FilesToDownload/Order.csv";
+    }
+
+
+    connection.connect(error => {
+        if (error) throw error;
+    
+        // query data from MySQL
+        connection.query('SELECT * from users', function(error, data, fields) {
+           
+            if (error) throw error;
+            const ws = fs.createWriteStream(PathToCSV);
+            const jsonData = JSON.parse(JSON.stringify(data));
+            
+    
+            fastcsv
+                .write(jsonData, { headers: true })
+                .on("finish", function() {    
+                }).pipe(ws);
+        });
+    });
+
+    fs.exists(PathToCSV, function(exists) {
+        if (exists) {
+            res.writeHead(200, {
+                "Content-Type": "application/octet-stream",
+                "Content-Disposition": "attachment; filename=" + "Order.csv"
+            });
+            fs.createReadStream(PathToCSV).pipe(res);
         } else {
             res.writeHead(400, { "Content-Type": "text/plain" });
             res.end("ERROR File does not exist");
