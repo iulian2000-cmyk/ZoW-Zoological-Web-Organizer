5ad6b249d8bb5cf92c794b70208e16b7564994e1
iulian2000-cmyk <67906958+iulian2000-cmyk@users.noreply.github.com>
2021-05-26 18:08:38 +0300

    Update chart_creator.js

--- ZoW Project/Backend/services/chart_creator.js
+++ ZoW Project/Backend/services/chart_creator.js
@@ -14,22 +14,22 @@ var  SQL_interogation ;
 
 if(process.platform == "win32"){
     SQL_interogation = "SELECT AVG(longevitate) AS "Medie_viata"," +
-    "(SELECT COUNT( * ) FROM animals WHERE TRIM(cardCategorie) = \"Mamifere\") AS \"Numar_mamifere\"," +
-    "(SELECT COUNT( * ) FROM animals WHERE insecta = 1) AS \"Numar_insecte\", " +
-    "(SELECT COUNT( * ) FROM animals WHERE terestru = 1) AS \"Numar_animale_terestre\"," +
-    "(SELECT COUNT( * ) FROM animals WHERE acvatic = 1) AS \"Numar_animale_acvatice\"," +
-    "(SELECT COUNT( * ) FROM animals WHERE aerian = 1) AS \"Numar_animale_aeriane\"," +
-    "(SELECT COUNT( * ) FROM animals WHERE domestic = 1) AS \"Numar_animale_domestice\"," +
-    "(SELECT COUNT( * ) FROM animals WHERE TRIM(cardModDeHranire) = \"erbivor\") AS \"Numar_animale_erbivore\"," +
-    "(SELECT COUNT( * ) FROM animals WHERE TRIM(cardModDeHranire) = \"carnivor\") AS \"Numar_animale_carnivore\"," +
-    "(SELECT COUNT( * ) FROM animals WHERE TRIM(cardModDeHranire) = \"omnivor\") AS \"Numar_animale_omnivore\"," +
-    "(SELECT COUNT( * ) FROM animals WHERE TRIM(cardModDeHranire) = \"parazitar\") AS \"Numar_animale_parazitare\"," +
-    "(SELECT AVG(longevitate) FROM ANIMALS WHERE TRIM(cardCategorie) = \"Mamifere\") AS \"Medie_viata_mamifere\" ," +
-    "(SELECT AVG(longevitate) FROM ANIMALS WHERE TRIM(cardCategorie) = \"Pasari\") AS \"Medie_viata_pasari\" ," +
-    "(SELECT AVG(longevitate) FROM ANIMALS WHERE TRIM(cardCategorie) = \"Insecte\") AS \"Medie_viata_insecte\" ," +
-    "(SELECT AVG(longevitate) FROM ANIMALS WHERE TRIM(cardCategorie) = \"Reptile\") AS \"Medie_viata_reptile\" ," +
-    "(SELECT AVG(longevitate) FROM ANIMALS WHERE TRIM(cardCategorie) = \"Dinozauri\") AS \"Medie_viata_dinozauri\" ," +
-    "(SELECT AVG(longevitate) FROM ANIMALS WHERE TRIM(cardCategorie) = \"Pesti\") AS \"Medie_viata_pesti\" ," +
+    "(SELECT COUNT(*) FROM animals WHERE TRIM(cardCategorie) = \"Mamifere\") AS \"Numar_mamifere\"," +
+    "(SELECT COUNT(*) FROM animals WHERE insecta = 1) AS \"Numar_insecte\", " +
+    "(SELECT COUNT(*) FROM animals WHERE terestru = 1) AS \"Numar_animale_terestre\"," +
+    "(SELECT COUNT(*) FROM animals WHERE acvatic = 1) AS \"Numar_animale_acvatice\"," +
+    "(SELECT COUNT(*) FROM animals WHERE aerian = 1) AS \"Numar_animale_aeriane\"," +
+    "(SELECT COUNT(*) FROM animals WHERE domestic = 1) AS \"Numar_animale_domestice\"," +
+    "(SELECT COUNT(*) FROM animals WHERE TRIM(cardModDeHranire) = \"erbivor\") AS \"Numar_animale_erbivore\"," +
+    "(SELECT COUNT(*) FROM animals WHERE TRIM(cardModDeHranire) = \"carnivor\") AS \"Numar_animale_carnivore\"," +
+    "(SELECT COUNT(*) FROM animals WHERE TRIM(cardModDeHranire) = \"omnivor\") AS \"Numar_animale_omnivore\"," +
+    "(SELECT COUNT(*) FROM animals WHERE TRIM(cardModDeHranire) = \"parazitar\") AS \"Numar_animale_parazitare\"," +
+    "(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Mamifere\") AS \"Medie_viata_mamifere\" ," +
+    "(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Pasari\") AS \"Medie_viata_pasari\" ," +
+    "(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Insecte\") AS \"Medie_viata_insecte\" ," +
+    "(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Reptile\") AS \"Medie_viata_reptile\" ," +
+    "(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Dinozauri\") AS \"Medie_viata_dinozauri\" ," +
+    "(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Pesti\") AS \"Medie_viata_pesti\" ," +
     "(SELECT COUNT( * ) FROM animals WHERE salbatic = 1) AS \"Numar_animale_salbatice\" FROM animals;";
 }else{
       SQL_interogation = "SELECT AVG(longevitate) AS \"Medie_viata\",(SELECT COUNT(*) FROM animals WHERE TRIM(cardCategorie) = \"Mamifere\") AS \"Numar_mamifere\",(SELECT COUNT(*) FROM animals WHERE insecta = 1) AS \"Numar_insecte\",(SELECT COUNT(*) FROM animals WHERE terestru = 1) AS \"Numar_animale_terestre\",(SELECT COUNT(*) FROM animals WHERE acvatic = 1) AS \"Numar_animale_acvatice\",(SELECT COUNT(*) FROM animals WHERE aerian = 1) AS \"Numar_animale_aeriane\",(SELECT COUNT(*) FROM animals WHERE domestic = 1) AS \"Numar_animale_domestice\",(SELECT COUNT(*) FROM animals WHERE TRIM(cardModDeHranire) = \"erbivor\") AS \"Numar_animale_erbivore\",(SELECT COUNT(*) FROM animals WHERE TRIM(cardModDeHranire) = \"carnivor\") AS \"Numar_animale_carnivore\",(SELECT COUNT(*) FROM animals WHERE TRIM(cardModDeHranire) = \"omnivor\") AS \"Numar_animale_omnivore\",(SELECT COUNT(*) FROM animals WHERE TRIM(cardModDeHranire) = \"parazitar\") AS \"Numar_animale_parazitare\",(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Mamifere\") AS \"Medie_viata_mamifere\" ,(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Pasari\") AS \"Medie_viata_pasari\" ,(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Insecte\") AS \"Medie_viata_insecte\" ,(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Reptile\") AS \"Medie_viata_reptile\" ,(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Dinozauri\") AS \"Medie_viata_dinozauri\" ,(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Pesti\") AS \"Medie_viata_pesti\" ,(SELECT COUNT(*) FROM animals WHERE salbatic = 1) AS \"Numar_animale_salbatice\" FROM animals;";
