413acbf669b95b735c582eca5a95508032167cbe
RobertaNechita <roberta.nechita@info.uaic.ro>
2021-05-28 11:36:00 +0300

    Update chart_creator.js

--- ZoW_Project/Backend/services/chart_creator.js
+++ ZoW_Project/Backend/services/chart_creator.js
@@ -23,6 +23,17 @@ if (process.platform == "win32") {
         "(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Reptile\") AS \"Medie_viata_reptile\" ," +
         "(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Dinozauri\") AS \"Medie_viata_dinozauri\" ," +
         "(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Pesti\") AS \"Medie_viata_pesti\" ," +
+        "(SELECT COUNT(*) FROM animals WHERE TRIM(cardModDeHranire) = \"parazitar\") AS \"Numar_animale_parazitare\"," +
+        "(SELECT AVG(greutate) FROM animals WHERE TRIM(cardCategorie) = \"Mamifere\") AS \"Greutate_medie_mamifere\" ," +
+        "(SELECT AVG(greutate) FROM animals WHERE TRIM(cardCategorie) = \"Pasari\") AS \"Greutate_medie_pasari\" ," +
+        "(SELECT AVG(greutate) FROM animals WHERE TRIM(cardCategorie) = \"Insecte\") AS \"Greutate_medie_insecte\" ," +
+        "(SELECT AVG(greutate) FROM animals WHERE TRIM(cardCategorie) = \"Reptile\") AS \"Greutate_medie_reptile\" ," +
+        "(SELECT AVG(greutate) FROM animals WHERE TRIM(cardCategorie) = \"Dinozauri\") AS \"Greutate_medie_dinozauri\" ," +
+        "(SELECT AVG(greutate) FROM animals WHERE TRIM(cardCategorie) = \"Pesti\") AS \"Greutate_medie_pesti\" ," +
+        "(SELECT AVG(inaltime) FROM animals WHERE TRIM(cardCategorie) = \"Mamifere\") AS \"Inaltime_medie_mamifere\" ," +
+        "(SELECT AVG(inaltime) FROM animals WHERE TRIM(cardCategorie) = \"Pasari\") AS \"Inaltime_medie_pasari\" ," +
+        "(SELECT AVG(inaltime) FROM animals WHERE TRIM(cardCategorie) = \"Dinozauri\") AS \"Inaltime_medie_dinozauri\" ," +
+        "(SELECT AVG(inaltime) FROM animals WHERE TRIM(cardCategorie) = \"Reptile\") AS \"Inaltime_medie_reptile\" ," +
         "(SELECT COUNT( * ) FROM animals WHERE salbatic = 1) AS \"Numar_animale_salbatice\" FROM animals;";
 } else {
     SQL_interogation = "SELECT AVG(longevitate) AS \"Medie_viata\",(SELECT COUNT(*) FROM animals WHERE TRIM(cardCategorie) = \"Mamifere\") AS \"Numar_mamifere\",(SELECT COUNT(*) FROM animals WHERE insecta = 1) AS \"Numar_insecte\",(SELECT COUNT(*) FROM animals WHERE terestru = 1) AS \"Numar_animale_terestre\",(SELECT COUNT(*) FROM animals WHERE acvatic = 1) AS \"Numar_animale_acvatice\",(SELECT COUNT(*) FROM animals WHERE aerian = 1) AS \"Numar_animale_aeriane\",(SELECT COUNT(*) FROM animals WHERE domestic = 1) AS \"Numar_animale_domestice\",(SELECT COUNT(*) FROM animals WHERE TRIM(cardModDeHranire) = \"erbivor\") AS \"Numar_animale_erbivore\",(SELECT COUNT(*) FROM animals WHERE TRIM(cardModDeHranire) = \"carnivor\") AS \"Numar_animale_carnivore\",(SELECT COUNT(*) FROM animals WHERE TRIM(cardModDeHranire) = \"omnivor\") AS \"Numar_animale_omnivore\",(SELECT COUNT(*) FROM animals WHERE TRIM(cardModDeHranire) = \"parazitar\") AS \"Numar_animale_parazitare\",(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Mamifere\") AS \"Medie_viata_mamifere\" ,(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Pasari\") AS \"Medie_viata_pasari\" ,(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Insecte\") AS \"Medie_viata_insecte\" ,(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Reptile\") AS \"Medie_viata_reptile\" ,(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Dinozauri\") AS \"Medie_viata_dinozauri\" ,(SELECT AVG(longevitate) FROM animals WHERE TRIM(cardCategorie) = \"Pesti\") AS \"Medie_viata_pesti\" ,(SELECT COUNT(*) FROM animals WHERE salbatic = 1) AS \"Numar_animale_salbatice\" FROM animals;";
@@ -60,6 +71,7 @@ connection.query({ sql: SQL_interogation, timeout: 1000 }, function(error, resul
 
     chart.setBackgroundColor("transparent");
     chart.toFile('chart.png');
+
     chart.setConfig({
         type: 'doughnut',
         data: {
@@ -102,6 +114,37 @@ connection.query({ sql: SQL_interogation, timeout: 1000 }, function(error, resul
 
     chart.setBackgroundColor("transparent");
     chart.toFile('chart4.png');
+
+
+    chart.setConfig({
+        type: 'bar',
+        data: {
+            labels: ['Mamifere', 'Pasari', 'Reptile', 'Dinozauri', 'Insecte', 'Pesti'],
+            datasets: [{
+                label: 'Greutatea medie a animalelor( kilograme )',
+                data: [results[0].Greutate_medie_mamifere, results[0].Greutate_medie_pasari, results[0].Greutate_medie_reptile, results[0].Greutate_medie_dinozauri, results[0].Greutate_medie_insecte, results[0].Greutate_medie_pesti]
+            }]
+        },
+    });
+
+    chart.setBackgroundColor("transparent");
+    chart.toFile('chart5.png');
+
+
+    
+    chart.setConfig({
+        type: 'bar',
+        data: {
+            labels: ['Mamifere', 'Pasari', 'Reptile', 'Dinozauri'],
+            datasets: [{
+                label: 'Inaltime medie a animalelor( metri )',
+                data: [results[0].Inaltime_medie_mamifere, results[0].Inaltime_medie_pasari, results[0].Inaltime_medie_reptile, results[0].Inaltime_medie_dinozauri]
+            }]
+        },
+    });
+
+    chart.setBackgroundColor("transparent");
+    chart.toFile('chart6.png');
     connection.destroy();
 
 });
