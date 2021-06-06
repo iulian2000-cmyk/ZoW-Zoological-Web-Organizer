2509beae69eed9844c597e1dda81d81c6207b0ba
Silviu <dumitru.silviu21@gmail.com>
2021-06-06 18:34:12 +0300

    updates

--- ZoW_Project/Backend/controllers/controller.js
+++ ZoW_Project/Backend/controllers/controller.js
@@ -53,49 +53,36 @@ module.exports = http.createServer((req, res) => {
     if (reqUrl.pathname.includes("ranking.html") && req.method === 'GET') {
         pageService.ranking_page(req, res);
     }
-
-    // GET endpoint
     if (reqUrl.pathname == '/FrontEnd/index.html' && req.method === 'GET') {
         pageService.mainPage(req, res);
     }
-    // GET endpoint 
     if ((reqUrl.pathname.includes("get_Order")) && (req.method === 'GET')) {
         download_service.download_xml(req, res);
     }
-
-    //GET endpoint
     if ((reqUrl.pathname.includes("get_CSV")) && (req.method === 'GET')) {
         download_service.download_csv(req, res);
     }
-
-    //GET endpoint
     if ((reqUrl.pathname.includes("get_PDF")) && (req.method === 'GET')) {
         download_service.download_pdf(req, res);
     }
-    // GET endpoint 
     if (reqUrl.pathname.includes(".css") && req.method === 'GET') {
         pageService.load_css(req, res);
     }
-    // GET endpoint
     if (reqUrl.pathname.includes(".js") && req.method === 'GET') {
         pageService.load_js(req, res);
     }
-    // GET endpoint 
     if ((reqUrl.pathname.includes(".png") || reqUrl.pathname.includes(".jpg")) && req.method === 'GET') {
         pageService.load_image(req, res);
     }
     if (reqUrl.pathname.includes(".jpeg")) {
         pageService.load_jpeg(req, res);
     }
-    // GET endpoint 
     if (reqUrl.pathname.includes(".svg") && req.method === 'GET') {
         pageService.load_svg(req, res);
     }
-    // POST endpoint
     if (reqUrl.pathname == '/FrontEnd/pages/auth' && req.method === 'POST') {
         login_service.login(req, res);
     }
-    // POST endpoint
     if (reqUrl.pathname == '/FrontEnd/pages/register' && req.method === 'POST') {
         login_service.register(req, res);
     }
--- ZoW_Project/Backend/services/generate_album_service.js
+++ ZoW_Project/Backend/services/generate_album_service.js
@@ -93,7 +93,7 @@ exports.generate_album = function(req, res) {
             }
         });
     } else if (domesticity === "none" && edibility === "none") {
-        connection.query(`SELECT id_animal, animalName, longevitate, likes, inaltime, greutate, imagePath1 from animals where ${category}=1 and (longevitate between ? and ?);`, [category, longevityArray[0], longevityArray[1]], function(error, results, fields) {
+        connection.query(`SELECT id_animal, animalName, longevitate, likes, inaltime, greutate, imagePath1 from animals where ${category}=1 and (longevitate between ? and ?);`, [longevityArray[0], longevityArray[1]], function(error, results, fields) {
             console.log('4 ' + error);
             if (results.length > 0) {
                 const response = [];
