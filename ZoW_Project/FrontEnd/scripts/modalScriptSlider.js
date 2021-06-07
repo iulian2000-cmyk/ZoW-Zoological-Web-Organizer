875b2d3ee41b0b5f5dba5e036e783684a3b280c2
RobertaNechita <roberta.nechita@info.uaic.ro>
2021-06-07 22:24:21 +0300

    modalUpdates

--- ZoW_Project/FrontEnd/scripts/modalScriptSlider.js
+++ ZoW_Project/FrontEnd/scripts/modalScriptSlider.js
@@ -18,21 +18,26 @@ window.onload = function() {
     });
 
 
-
-
     var modal = document.getElementById("aboutModal");
 
     var btn = document.getElementById("aboutBtn");
 
     var span = document.getElementsByClassName("closebutton")[0];
 
-    btn.onclick = () => modal.style.display = "block";
-
-    span.onclick = () => modal.style.display = "none";
-
+    btn.onclick = () => {
+        modal.style.display = "block";
+        if (document.getElementsByClassName("navBar")) {
+            document.getElementsByClassName("navBar")[0].style.zIndex = 1;
+        }
+    }
+    span.onclick = () => {
+        modal.style.display = "none";
+        document.getElementsByClassName("navBar")[0].style.zIndex = 2;
+    }
     window.onclick = (event) => {
         if (event.target == modal) {
             modal.style.display = "none";
+            document.getElementsByClassName("navBar")[0].style.zIndex = 2;
         }
     }
 
